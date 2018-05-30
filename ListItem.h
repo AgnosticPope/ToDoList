64d059c1c7d54797e19fb2038d1ee57dfe0b91ef
megwest <39739319+megwest@users.noreply.github.com>
2018-05-29 19:51:00 -0500

    Uploaded files
    Uploaded node, listItem, main

---
+++ ListItem.h
@@ -0,0 +1,34 @@
+#ifndef LISTITEM_H
+#define LISTITEM_H
+
+#include "Node.h"
+
+template<class ItemType>
+class ListItem : public Node<ItemType>
+{
+private:
+	int priorityValue;		//indicates item’s priority in the list
+	bool completed;		//check mark for if the task has been completed yet
+
+public:
+	
+	ListItem();
+	ListItem(int value, ItemType &desc);
+	int getPriorityValue() const;
+	bool setPriorityValue(int value);
+	bool getCompleted() const;
+	bool setCompleted();
+	bool operator==(const ListItem &li);
+	bool operator<=(const ListItem &li);
+	bool operator>=(const ListItem &li);
+	bool operator!=(const ListItem &li);
+
+	//inherited from Node
+	void setItem(const ItemType& anItem);
+	void setNext(Node<ItemType>* nextNodePtr);
+	ItemType getItem() const;
+	Node<ItemType>* getNext() const;
+
+}; // end ListItem
+
+#endif
---
+++ Node.h
@@ -0,0 +1,24 @@
+
+/** @file Node.h
+Listing 4-1 */
+#ifndef NODE_
+#define NODE_
+
+template<class ItemType>
+class Node
+{
+private:
+	ItemType        description; // A data item
+	Node<ItemType>* next; // Pointer to next node
+
+public:
+	Node();
+	Node(const ItemType& desc);
+	Node(const ItemType& desc, Node<ItemType>* nextNodePtr);
+	void setDescription(const ItemType& desc);
+	void setNext(Node<ItemType>* nextNodePtr);
+	ItemType getDescription() const;
+	Node<ItemType>* getNext() const;
+}; // end Node
+
+#endif
